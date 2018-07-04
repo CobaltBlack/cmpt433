483b545cde9d74456b664554561dc4efb5f1aeb6
Eric <007.ericliu@gmail.com>
2018-07-04 13:10:40 -0700

    Hook up front end buttons to send commands

--- work/as3/as3-server/public/javascripts/beatbox_ui.js
+++ work/as3/as3-server/public/javascripts/beatbox_ui.js
@@ -3,6 +3,9 @@
 	DOM Basics - Showing how to use the DOM.
 */
 
+const POLL_INTERVAL_MS = 5000
+const COMMAND_DELIM = ";"
+
 var socket = io.connect();
 
 // Run when webpage fully loaded
@@ -10,25 +13,87 @@ $(document).ready(function() {
 
 	// Define button behaviours
 
-	// Register a callback function for the changeBtn button:
-	$('#modeNone').click(function() {
-		// Log a message and call other function.
-		console.log("Clicked NONE!");
-		console.log("Hello world!");
-		sendServerCommand("hello server");
-	});
+	// Register callback functions for each button
+	$('#modeNone').click(	function() { sendServerCommand("mode none"); 	});
+	$('#modeRock1').click(	function() { sendServerCommand("mode rock1"); 	});
+	$('#modeRock2').click(	function() { sendServerCommand("mode rock2"); 	});
+	$('#volumeUp').click(	function() { sendServerCommand("volume up"); 	});
+	$('#volumeDown').click(	function() { sendServerCommand("volume down"); 	});
+	$('#bpmUp').click(		function() { sendServerCommand("bpm up"); 		});
+	$('#bpmDown').click(	function() { sendServerCommand("bpm down"); 	});
+	$('#playHihat').click(	function() { sendServerCommand("play hihat"); 	});
+	$('#playSnare').click(	function() { sendServerCommand("play snare"); 	});
+	$('#playBass').click(	function() { sendServerCommand("play bass"); 	});
 
 	// Incoming control messages
 	socket.on('serverReply', function(data) {
-		handleServerCommand(data);
+		handleServerCommands(data);
 	});
 
 	// Outgoing control messages
 	
+	// Poll server for new data
+	window.setTimeout(pollServer, POLL_INTERVAL_MS);
 });
 
-function handleServerCommand(data) {
+function pollServer() {
+	sendServerCommand("update");
+}
+
+// Handles multiple commands seperated by COMMAND_DELIM
+function handleServerCommands(data) {
 	console.log("received server command:", data);
+	var commands = data.split(COMMAND_DELIM);
+	for (command in commands) {
+		handleServerCommand(command);
+	}
+}
+
+// Handles single command
+function handleServerCommand(command) {
+	var parsedWords = command.split(" ");
+	if (parsedWords.length == 0) {
+		return;
+	}
+
+	var primaryCommand = parsedWords[0];
+
+	if (primaryCommand == "") {
+		
+	}
+	else if (primaryCommand == "") {
+		
+	}
+	else if (primaryCommand == "") {
+		
+	}
+	else if (primaryCommand == "") {
+		
+	}
+	else if (primaryCommand == "") {
+		
+	}
+	else if (primaryCommand == "") {
+		
+	}
+	else if (primaryCommand == "") {
+		
+	}
+	else if (primaryCommand == "") {
+		
+	}
+	else if (primaryCommand == "") {
+		
+	}
+	else if (primaryCommand == "") {
+		
+	}
+	else if (primaryCommand == "") {
+		
+	}
+	else {
+		console.log("Error: Unrecognized command");
+	}
 }
 
 function sendServerCommand(data) {
