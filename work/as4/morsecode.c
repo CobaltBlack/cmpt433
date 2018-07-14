f99d6b52f4739aa44a9411010bc631c2883ab479
Eric Liu <007.ericliu@gmail.com>
2018-07-14 14:48:40 -0700

    Blinking morse code using LED when write()ing to driver file

--- work/as4/morsecode.c
+++ work/as4/morsecode.c
@@ -9,10 +9,23 @@ by Feng Liu - 301218282 - liufengl@sfu.ca
 #include <linux/miscdevice.h> // for misc-driver calls.
 #include <linux/delay.h>	// msleep()
 #include <linux/fs.h>
+#include <asm/uaccess.h>
 
 //#error Are we building this?
 
-#define MY_DEVICE_FILE  "corse_code"
+#define MY_DEVICE_FILE  "morse-code"
+
+/**************************************************************
+ * FIFO Support
+ *************************************************************/
+// Info on the interface:
+//    https://www.kernel.org/doc/htmldocs/kernel-api/kfifo.html#idp10765104
+// Good example:
+//    http://lxr.free-electrons.com/source/samples/kfifo/bytestream-example.c
+#include <linux/kfifo.h>
+#define FIFO_SIZE 256	// Must be a power of 2.
+static DECLARE_KFIFO(my_fifo, char, FIFO_SIZE);
+
 
 /******************************************************
  * Parameter
@@ -20,6 +33,11 @@ by Feng Liu - 301218282 - liufengl@sfu.ca
 #define DEFAULT_DOTTIME 200
 #define MIN_DOTTIME 0
 #define MAX_DOTTIME 2000
+
+#define CHAR_GAP_DOTTIMES 3
+#define WORD_GAP_DOTTIMES 7
+
+#define WORD_GAP_NUM_SPACES 3
 
 static int dottime = DEFAULT_DOTTIME;
 
@@ -32,30 +50,64 @@ MODULE_PARM_DESC(dottime, "Dottime in milliseconds");
  ******************************************************/
 #include <linux/leds.h>
 
-DEFINE_LED_TRIGGER(ledtrig_demo);
+DEFINE_LED_TRIGGER(morsecode_ledtrig);
 
-#define LED_ON_TIME_ms 100
-#define LED_OFF_TIME_ms 900
-
-static void my_led_blink(void)
+static void led_setEnabled(int isEnabled)
 {
-	led_trigger_event(ledtrig_demo, LED_FULL);
-	msleep(LED_ON_TIME_ms);
-	led_trigger_event(ledtrig_demo, LED_OFF);
-	msleep(LED_OFF_TIME_ms);
+	if (isEnabled) {
+		led_trigger_event(morsecode_ledtrig, LED_FULL);
+	}
+	else {
+		led_trigger_event(morsecode_ledtrig, LED_OFF);
+	}
 }
 
 static void led_register(void)
 {
 	// Setup the trigger's name:
-	led_trigger_register_simple("demo", &ledtrig_demo);
+	led_trigger_register_simple("morse-code", &morsecode_ledtrig);
 }
 
 static void led_unregister(void)
 {
 	// Cleanup
-	led_trigger_unregister_simple(ledtrig_demo);
+	led_trigger_unregister_simple(morsecode_ledtrig);
 }
+
+
+/******************************************************
+ * Morse code
+ ******************************************************/
+#define BITS_PER_BYTE 8
+
+static unsigned short morsecode_codes[] = {
+		0xB800,	// A 1011 1
+		0xEA80,	// B 1110 1010 1
+		0xEBA0,	// C 1110 1011 101
+		0xEA00,	// D 1110 101
+		0x8000,	// E 1
+		0xAE80,	// F 1010 1110 1
+		0xEE80,	// G 1110 1110 1
+		0xAA00,	// H 1010 101
+		0xA000,	// I 101
+		0xBBB8,	// J 1011 1011 1011 1
+		0xEB80,	// K 1110 1011 1
+		0xBA80,	// L 1011 1010 1
+		0xEE00,	// M 1110 111
+		0xE800,	// N 1110 1
+		0xEEE0,	// O 1110 1110 111
+		0xBBA0,	// P 1011 1011 101
+		0xEEB8,	// Q 1110 1110 1011 1
+		0xBA00,	// R 1011 101
+		0xA800,	// S 1010 1
+		0xE000,	// T 111
+		0xAE00,	// U 1010 111
+		0xAB80,	// V 1010 1011 1
+		0xBB80,	// W 1011 1011 1
+		0xEAE0,	// X 1110 1010 111
+		0xEBB8,	// Y 1110 1011 1011 1
+		0xEEA0	// Z 1110 1110 101
+};
 
 
 /******************************************************
@@ -64,14 +116,81 @@ static void led_unregister(void)
 static ssize_t my_read(struct file *file,
 		char *buff, size_t count, loff_t *ppos)
 {
-	my_led_blink();
+	printk(KERN_INFO "<---- Morse code my_read().\n");
 	return 0;
 }
 
 static ssize_t my_write(struct file *file,
 		const char *buff, size_t count, loff_t *ppos)
 {
-	return 0;
+	int i, j, k, chIndex, codeEndIndex;
+	int numBits;
+	unsigned short code;
+	unsigned short bit;
+
+	numBits = BITS_PER_BYTE * sizeof(unsigned short);
+
+	printk(KERN_INFO "<---- my_write() - numBits=%d./\n", numBits);
+
+	for (i = 0; i < count; i++) {
+		char ch;
+
+		if (copy_from_user(&ch, &buff[i], sizeof(ch))) {
+			return -EFAULT;
+		}
+
+		if (ch == ' ') {
+			// Sleep for space between words
+			msleep(dottime * WORD_GAP_DOTTIMES);
+
+			// Add spaces to the FIFO
+			for (j = 0; j < WORD_GAP_NUM_SPACES: j++) {
+				if (!kfifo_put(&my_fifo, ' ')) {
+					return -EFAULT;
+				}
+			}
+		}
+		else if ('A' <= ch && ch <= 'Z') {
+			chIndex = ch - 'A';
+			code = morsecode_codes[chIndex];
+		}
+		else if ('a' <= ch && ch <= 'z') {
+			chIndex = ch - 'a';
+			code = morsecode_codes[chIndex];
+		}
+
+		printk(KERN_INFO "<---- Morse code process [%c].\n", ch);
+
+		// Find at what index the morse code ends
+		for (k = 0; k < numBits; k++) {
+			bit = (code & ( 1 << k )) >> k;
+
+			if (bit) {
+				codeEndIndex = k;
+				break;
+			}
+		}
+
+		// Read the morse code starting at MSB until the end index
+		for (k = numBits - 1; k >= codeEndIndex; k--) {
+			bit = (code & ( 1 << k )) >> k;
+
+			led_setEnabled(bit);
+			msleep(dottime);
+
+			// TODO: Insert dash/dot/space into FIFO
+		}
+
+		// Sleep for gap between letters
+		led_setEnabled(0);
+		msleep(dottime * CHAR_GAP_DOTTIMES);
+
+		// Insert dot/dash into fifo
+
+	}
+
+	*ppos += count;
+	return count;
 }
 
 
@@ -99,7 +218,7 @@ static struct miscdevice my_miscdevice = {
 static int __init morsedriver_init(void)
 {
 	int ret;
-	
+
 	printk(KERN_INFO "----> Morse code driver init()\n");
 
 	if (dottime < MIN_DOTTIME || MAX_DOTTIME < dottime) {
@@ -110,6 +229,8 @@ static int __init morsedriver_init(void)
 	ret = misc_register(&my_miscdevice);
 
 	led_register();
+
+	INIT_KFIFO(my_fifo);
 
 	return ret;
 }
